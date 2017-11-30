9242489e10830477bfae59650380766eedce55ab
DESKTOP-65FF32R\yk2 <koogi012@gmail.com>
2017-11-30 16:49:40 +0900

    Ajax를 통한 데이터 수정, 삭제 리펙토리

--- src/main/java/net/slipp/domain/Question.java
+++ src/main/java/net/slipp/domain/Question.java
@@ -1,39 +1,37 @@
 package net.slipp.domain;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Entity
-public class Question {
-	
-	@Id
-	@GeneratedValue
-	private Long id;
-	
+public class Question extends AbstractEntity {
+
 	@ManyToOne
 	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
+	@JsonProperty
 	private User   writer;
 	
+	@JsonProperty
 	private String title;
 	
 	@Lob
+	@JsonProperty
 	private String contents;
 	
-	private LocalDateTime	createDate;
+	@JsonProperty
+	private Integer countOfAnswer = 0;
 	
 	@OneToMany(mappedBy="question")
-	@OrderBy("id ASC")
+	@OrderBy("id DESC")
 	private List<Answer> answers;
 	
 	public Question() {};
@@ -43,16 +41,8 @@ public class Question {
 		this.writer = writer;
 		this.title = title;
 		this.contents = contents;
-		
-		this.createDate = LocalDateTime.now();
 	}
 	
-	public String getFormattedCreateDate() {
-		if (this.createDate == null) return "";
-		
-		return this.createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
-	}
-
 	 public void update(String title, String contents) {
 		this.title = title;
 		this.contents = contents;
@@ -61,5 +51,20 @@ public class Question {
 	public boolean isSameWriter(User loginUser) {
 		return this.writer.equals(loginUser);
 	}
-		
+	
+	public void addAnswer() {
+		this.countOfAnswer += 1; 
+	}
+
+	public void deleteAnswer() {
+		this.countOfAnswer -= 1; 
+	}
+
+	@Override
+	public String toString() {
+		return "Question [" + super.toString() + ", writer=" + writer + ", title=" + title + ", contents=" + contents + ", countOfAnswer="
+				+ countOfAnswer + ", answers=" + answers + "]";
+	}
+	
+	
 }
--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -2,33 +2,28 @@ package net.slipp.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
-public class User {
-	
-	@Id
-	@GeneratedValue
-	private Long   id; 
+public class User extends AbstractEntity {
 	
 	@Column(nullable=false, length=20, unique=true)
+	@JsonProperty
 	private String userId;
+	@JsonIgnore
 	private String password;
+	@JsonProperty
 	private String name;
+	@JsonProperty
 	private String email;
 
-	public Long getId() {
-		return id;
-	}
-	
-	public boolean matchId( Long Id ) {
+	public boolean matchId( Long newId ) {
 		
-		if ( Id == null ) return false;
+		if ( newId == null ) return false;
 		
-		if (!this.id.equals(Id)) return false;
-		
-		return true;
+		return newId.equals(getId());
 	}
 
 	public void setUserId(String userId) {
@@ -70,40 +65,9 @@ public class User {
 		this.email = newUser.email;
 
 	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 
 	@Override
 	public String toString() {
-		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+		return "User [" + super.toString() + ", userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
 	}
-
-
-
-	
-	
-
 }
